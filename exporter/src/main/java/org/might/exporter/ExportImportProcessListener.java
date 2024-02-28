6ff30ab341dda623b0c28661a1327567125282bd
Andrei F._ <answers_com@hotmail.com>
2024-02-28 21:24:30 +0300

    Added licence header.

--- exporter/src/main/java/org/might/exporter/ExportImportProcessListener.java
+++ exporter/src/main/java/org/might/exporter/ExportImportProcessListener.java
@@ -1,3 +1,26 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2024 Andrei F._
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 package org.might.exporter;
 
 import com.liferay.exportimport.kernel.lar.PortletDataContext;
@@ -37,7 +60,6 @@ public class ExportImportProcessListener implements ProcessAwareExportImportLife
     private static final String GLOBAL_SUCCESS_IMPORT_LAR = "Global " + SUCCESS_IMPORT_LAR;
     private static final String START_EXPORT = "Start export process";
     private static final String STOP_EXPORT = "Stop export process";
-    private static final String SKIP_EXPORT = "Skip export process based on the build installation";
     private static final String MAP_FIELD = "cmd";
     private static final String IMPORT_TYPE_CMD = "[import]";
     private static final String IMPORT_TYPE = "imported";
--- exporter/src/main/java/org/might/exporter/impl/FileZipUploaderImpl.java
+++ exporter/src/main/java/org/might/exporter/impl/FileZipUploaderImpl.java
@@ -1,3 +1,26 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2024 Andrei F._
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 package org.might.exporter.impl;
 
 import com.liferay.portal.kernel.util.PropsUtil;
@@ -82,9 +105,8 @@ public class FileZipUploaderImpl implements Uploader {
 
         LOGGER.info(START_UPLOAD_FILE, exportFolder);
         try {
-            Path tempFile = null;
             String prefixName = getPrefixName();
-            tempFile = getTempFilePath(prefixName);
+            Path tempFile = getTempFilePath(prefixName);
             LOGGER.info(TEMP_FILE_CREATED, tempFile.getFileName(), tempFile.getParent().getFileName());
             ZipOutputStream zipOutputStream = new ZipOutputStream(Files.newOutputStream(tempFile));
             zipOutputStream.putNextEntry(new ZipEntry(getFileName(prefixName, FILE_SUFFIX_NAME)));
